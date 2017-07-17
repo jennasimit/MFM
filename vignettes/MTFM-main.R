3540cc6ac650f09d81aa085eb990a94f1a4bd7b2
jennasimit <jennasimit@gmail.com>
2017-07-17 22:57:39 +0100

    Add files via upload

--- vignettes/MTFM-main.R
+++ vignettes/MTFM-main.R
@@ -15,7 +15,7 @@ mppthr=0.001
 
 # specify minimum and maximum model sizes for traits 1 and 2
 mT1=0; MT1=3
-mT2=0; mT2=3
+mT2=0; MT2=3
 
 # specify directory to save files
 mydir <- "tmp"
@@ -23,13 +23,13 @@ mydir <- "tmp"
 # specify sharing scales
 shared <- c(1,5,10,20,100,1000)
 
-tags <- make.tags.fn(r2=r2,G0=,mydir=NA) # set mydir=NA if do not want to save tags to an Rdata file
+tags <- make.tags.fn(r2=r2,G0=t1snp.data[t1pheno==0,],mydir=NA) # set mydir=NA if do not want to save tags to an Rdata file
 
 # find joint BFs and then posterior probabilities for tag SNP models
 ppbf1 <- PPBF.tags.fn(t1snp.data,t1pheno,t2snp.data,t2pheno,mydir,tags,mppthr,mT1,MT1,mT2,MT2,trait1,trait2)
 
 # expand tag SNP models 
-bf <- BFexpanded.fn(ppbf,tags,t1snp.data,t1pheno,t2snp.data,t2pheno,trait1,trait2)
+bf <- BFexpanded.fn(ppbf1,tags,t1snp.data,t1pheno,t2snp.data,t2pheno,trait1,trait2)
 
 
 PPfinal <- PPshared.fn(shared,bf,tags,trait1,trait2,mydir)
