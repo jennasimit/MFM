6942f1edd7b23ffa0775aab4b0850aeb26817f78
chr1swallace <cew54@cam.ac.uk>
2017-07-18 13:32:56 +0100

    exported marginalpp

--- R/marginalpp.R
+++ R/marginalpp.R
@@ -14,6 +14,112 @@
 ##'     assuming ABF for all other models are approximately 1),
 ##' shared
 ##' @author Chris Wallace
+marginalpp <- function(M, ABF, pr, kappa, p0) {
+    #1, M2, M3, ABF1, ABF2, ABF3, pr1, pr2, pr3, S, p0) {
+    ## are any of M1, M2 the null model?
+    n <- length(M)
+    if(n<2)
+        stop("Need at least 2 diseases")
+    if( length(ABF)!=n || length(pr)!=n )
+        stop("M, ABF and pr need to have the same lengths")
+    ncols <- sapply(M,function(x) dim(x)[2])
+    if(sd(ncols)!=0)
+        stop("all matrices in M must have the same SNPs (columns)")
+
+    ## remove null model if included
+    for(i in seq_along(M)) {
+        wh <- which.null(M[[i]])
+        if(length(wh)) {
+            M[[i]] <- M[[i]][-wh,]
+            ABF[[i]] <- ABF[[i]][-wh]
+        }
+    }
+
+    ## unweighted pp
+    pp <- mapply(function(pr1,ABF1) { calcpp(addnull(pr1,p0),addnull(ABF1,0)) },
+                 pr, ABF, SIMPLIFY=FALSE)
+
+    ## Q
+    fun <- switch(n,
+                  NULL,
+                  "calcQ2",
+                  "calcQ3")
+    M <- lapply(M,t)
+    if(is.null(fun))
+        stop("calcQ not written for ",n," diseases yet")
+
+    Q <- do.call(fun, c(M, lapply(pp,"[",-1)))
+
+    ## alt prior
+    app <- vector("list",n)
+    for(i in seq_along(Q)) {
+        tmp <- lapply(kappa, function(k) {
+            a <- pr[[i]] * (1 + (k-1) * Q[[i]])
+            a/sum(a)
+        })
+        tmp <- (1-p0) * do.call("cbind",tmp)
+        M[[i]] <- addnull(M[[i]],0)
+        app[[i]] <- calcpp(addnull(tmp,p0),addnull(ABF2,0))
+    }
+
+    maxpower <- n * (n-1) / 2
+    app <- vector("list",n)
+    for(i in seq_along(Q)) {
+        tmp <- lapply(kappa, function(k) {
+            s <- k^(1:maxpower)/maxpower
+            a <- pr[[i]] * (1 + t((s-1) * t(Q[[i]])))
+            rowSums(a)/sum(a)
+        })
+        tmp <- (1-p0) * do.call("cbind",tmp)
+        M[[i]] <- addnull(M[[i]],0)
+        app[[i]] <- calcpp(addnull(tmp,p0),addnull(ABF2,0))
+    }
+
+    M1 <- addnull(M1,0)
+    app1 <- calcpp(addnull(altpr1,p0),addnull(ABF1,0)) 
+
+    altpr2 <- lapply(S, function(s) {
+        a <- pr2 * (1 + (s-1) * Q[[2]])
+        a/sum(a)
+    })
+    M2 <- addnull(M2,0)
+    altpr2 <- (1-p0) * do.call("cbind",altpr2) 
+    app2 <- calcpp(addnull(altpr2,p0),addnull(ABF2,0)) 
+
+    altpr3 <- lapply(S, function(s) {
+        a <- pr3 * (1 + (s-1) * Q[[2]])
+        a/sum(a)
+    })
+    M2 <- addnull(M2,0)
+    altpr2 <- (1-p0) * do.call("cbind",altpr2) 
+    app2 <- calcpp(addnull(altpr2,p0),addnull(ABF2,0)) 
+    list(single.pp1=pp1,shared.pp1=app1,M1=M1,
+         single.pp2=pp2,shared.pp2=app2,M2=M2,
+         S=S)
+}
+
+which.null <- function(M) {
+    rs <- rowSums(M)
+    which(rs==0)
+}
+
+##' Calculate marginal model posterior probabilities for each disease 
+##'
+##' @title Marginal PP for models sharing information between diseases
+##' @param M1 model matrix for disease 1
+##' @param M2 model matrix for disease 2
+##' @param ABF1 log ABF for models in M1 for disease 1
+##' @param ABF2 log ABF for models in M2 for disease 2
+##' @param pr1 prior for models in M1
+##' @param pr2 prior for models in M2
+##' @param S single value or vector of values to consider for the
+##'     sharing scale parameter
+##' @param p0 prior probability of the null model
+##' @return list of single.pp1 (pp for each model in M1 for disease 1,
+##'     assuming ABF for all other models are approximately 1),
+##' shared
+##' @export
+##' @author Chris Wallace
 marginalpp2 <- function(M1, M2, ABF1, ABF2, pr1, pr2, S, p0) {
     ## are any of M1, M2 the null model?
     rs1 <- rowSums(M1)
